b88bf67da18bdc052b8ee533add3a621dfa5a005
Siraj Rauff <sirajrauff@gmail.com>
2019-10-21 17:17:02 -0400

    Adding Terraform for Azure Networking layer

---
+++ terraform/infrastructure/networking/networking.tf
@@ -0,0 +1,27 @@
+terraform {
+  backend "azurerm" {
+    # Variables are not allowed in "backend" blocks
+    resource_group_name  = "liftoff-modern-application-delivery"
+    storage_account_name = "liftoffmodernapplication"
+    container_name       = "tfstate"
+    key                  = "networking.tfstate"
+  }
+}
+
+data "azurerm_resource_group" "resource_group" {
+  name = var.resource_group_name
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = format("%s-network", data.azurerm_resource_group.resource_group.name)
+  address_space       = ["10.10.0.0/16"]
+  location            = data.azurerm_resource_group.resource_group.location
+  resource_group_name = data.azurerm_resource_group.resource_group.name
+}
+
+resource "azurerm_subnet" "public" {
+  name                 = "public"
+  resource_group_name  = data.azurerm_resource_group.resource_group.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefix       = "10.10.0.0/24"
+}
---
+++ terraform/infrastructure/networking/variables.tf
@@ -0,0 +1,14 @@
+////////////////////////////
+///// Azure
+
+variable "resource_group_name" {
+  type        = string
+  description = "The Azure Resource Group where resources will exist."
+  default     = "liftoff-modern-application-delivery"
+}
+
+variable "storage_account_name" {
+  type        = string
+  description = "The name of the Azure Storage Account to use."
+  default     = "liftoffmodernapplication"
+}
