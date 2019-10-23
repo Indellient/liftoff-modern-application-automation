4d96d907f0dd1169f47e65695f1e27556a60ef87
Siraj Rauff <sirajrauff@gmail.com>
2019-10-23 15:37:33 -0400

    Refactoring install-habitat.sh
    - Installing & Enabling Habitat Supervisor with 'accept-no-persist' license
    - Move Install detection inside of _install_hab function
    - More logging

--- packer/scripts/setup-habitat.sh
+++ packer/scripts/setup-habitat.sh
@@ -1,16 +1,36 @@
 #!/bin/bash -e
 
+echo "Starting ${0}"
 export HAB_LICENSE=accept-no-persist
 
 _install_hab(){
-    echo "Installing Habitat..."
     local __HAB_VERSION=${1:-}
 
     # Setup Installation Arguments
     __INSTALL_ARGS=""
     if [ -n "${__HAB_VERSION}" ]; then
-        echo "Found version ${__HAB_VERSION} supplied for Habitat installation"
+        echo "Found required version ${__HAB_VERSION} supplied for Habitat installation"
         __INSTALL_ARGS+=" -v ${__HAB_VERSION}"
+    fi
+
+    # Install habitat if it is not installed, or HAB_VERSION is supplied and does not match the hab version
+    if command -v hab &> /dev/null; then
+        local __INSTALLED_VERSION=$(hab --version)
+        echo "Found Habitat installation, version ${__INSTALLED_VERSION}"
+
+        if [ -z ${__HAB_VERSION} ]; then
+            echo "No Habitat version provided"
+            echo "You can provide a Habitat version using environment variable HAB_VERSION"
+            echo "Skipping Habitat installation..."
+            return 0
+        elif [[ "${__INSTALLED_VERSION}" != *"${__HAB_VERSION}"* ]]; then
+            echo "Installed version does not match required version ${__HAB_VERSION}"
+        else
+            echo "Installed Habitat version matches required version ${__HAB_VERSION}"
+            return 0
+        fi
+    else
+        echo "Habitat installation not found"
     fi
 
     # Install with arguments
@@ -24,25 +44,28 @@ _install_hab(){
     fi
 }
 
-# Install habitat if it is not installed, or HAB_VERSION is supplied and does not match the hab version
-if command -v hab &> /dev/null; then
-    __INSTALLED_VERSION=$(hab --version)
-    echo "Found Habitat installation, version ${__INSTALLED_VERSION}"
+_install_habitat_supervisor() {
+    echo "Preparing Habitat Supervisor..."
+    local __SUPERVISOR_SERVICE_NAME=${1:-hab-supervisor}
 
-    if [ -z ${HAB_VERSION} ]; then
-        echo "No Habitat version provided"
-        echo "You can provide a Habitat version using environment variable HAB_VERSION"
-        echo "Skipping Habitat installation..."
-    elif [[ "${__INSTALLED_VERSION}" != *"${HAB_VERSION}"* ]]; then
-        echo "Installed version does not match required version ${HAB_VERSION}"
-        _install_hab "${HAB_VERSION}"
-    else
-        echo "Installed Habitat version matches required version ${HAB_VERSION}"
-    fi
-else
-    echo "Hab installation not found"
-    _install_hab "${HAB_VERSION}"
-fi
+    sudo bash -c "cat > /etc/systemd/system/${__SUPERVISOR_SERVICE_NAME}.service" <<SERVICE
+Description=Habitat Supervisor
+
+[Service]
+Environment=HAB_LICENSE=accept-no-persist
+ExecStart=/bin/hab sup run
+Restart=on-failure
+
+[Install]
+WantedBy=network.target
+SERVICE
+
+    sudo systemctl daemon-reload
+    sudo systemctl enable ${__SUPERVISOR_SERVICE_NAME}
+}
+
+_install_hab "${HAB_VERSION}"
+_install_habitat_supervisor
 
 # Add hab user if it does not exist
 if ! id hab &>/dev/null; then
