967a054b1c04c151cfbfe7680b8b5f62af5619be
Siraj Rauff <sirajrauff@gmail.com>
2019-10-23 22:38:33 -0400

    Install Terraform on Jenkins

--- terraform/infrastructure/jenkins/main.tf
+++ terraform/infrastructure/jenkins/main.tf
@@ -141,6 +141,8 @@ sudo keytool -importkeystore \
   -srcstorepass ${random_password.key_store_password.result} \
   -deststorepass ${random_password.key_store_password.result}
 
+# Using custom terraform to update to 12.12, usually "core/terraform" is enough
+sudo hab pkg install -b liftoff-modern-application-delivery/terraform
 EOF
     ]
   }
