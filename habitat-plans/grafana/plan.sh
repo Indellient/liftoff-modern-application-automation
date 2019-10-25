18c76758ceeab1d4659c2decaa2748051cb5da4c
Siraj Rauff <sirajrauff@gmail.com>
2019-10-25 19:32:35 -0400

    Downgrading regular Grafana fixup

--- habitat-plans/grafana/plan.sh
+++ habitat-plans/grafana/plan.sh
@@ -1,21 +1,29 @@
 pkg_name=grafana
-pkg_origin=liftoff-modern-application-delivery
+pkg_origin=core
 pkg_version="4.6.3"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_license=("Apache-2.0")
-pkg_upstream_url=https://grafana.com
+pkg_license=('Apache-2.0')
+pkg_upstream_url=https://grafana.com/
 pkg_source="https://s3-us-west-2.amazonaws.com/${pkg_name}-releases/release/${pkg_name}-${pkg_version}.linux-x64.tar.gz"
 pkg_filename="${pkg_name}-${pkg_version}.linux-x64.tar.gz"
 pkg_shasum="037cf7678858c4a5b3632de7bea4c20475e081ff48c27c868be71e7b0a07c505"
-pkg_bin_dirs=("bin")
-pkg_deps=(core/glibc core/bash core/wget core/curl)
+pkg_deps=(core/glibc core/bash core/wget core/curl core/cacerts)
 pkg_build_deps=(core/patchelf)
+pkg_bin_dirs=(bin)
+pkg_description="Grafana graphing app, dynamically finds prometheus data sources"
+pkg_svc_user="root"
+pkg_svc_group=$pkg_svc_user
 pkg_exports=(
-  [grafana_port]=http_port
+  [grafana_port]=listening_port
 )
-pkg_exposes=(grafana_port)
-pkg_svc_user=root
 
+pkg_exposes=(grafana_port)
+
+pkg_binds_optional=(
+  [prom]="prom_ds_http"
+)
+
+# we're using prebuilt binaries; no build stage required
 do_build() {
   return 0
 }
@@ -23,11 +31,12 @@ do_build() {
 do_install() {
   mkdir -p "${pkg_svc_var_path}/plugins"
   cd "$HAB_CACHE_SRC_PATH/${pkg_name}-${pkg_version}" || exit 1
-  cp -r conf  public  scripts  tools "${pkg_prefix}/"
+  cp -r conf  public  scripts  vendor "${pkg_prefix}/"
   cp -r bin/* "${pkg_prefix}/bin/"
   patchelf --interpreter "$(pkg_path_for glibc)/lib/ld-linux-x86-64.so.2" \
-    "${pkg_prefix}/bin/grafana-server"
+         "${pkg_prefix}/bin/grafana-server"
   patchelf --interpreter "$(pkg_path_for glibc)/lib/ld-linux-x86-64.so.2" \
-    "${pkg_prefix}/bin/grafana-cli"
+         "${pkg_prefix}/bin/grafana-cli"
+  #install_cacerts
   return 0
 }
