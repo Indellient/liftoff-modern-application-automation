6b29b2dee0df73112d1f38706f482a5de0add67b
Siraj Rauff <sirajrauff@gmail.com>
2019-10-26 20:11:20 -0400

    Using Remote State for Vault Address

--- examples/example-3-secrets-management/terraform-pipeline/vault.tf
+++ examples/example-3-secrets-management/terraform-pipeline/vault.tf
@@ -1,3 +1,7 @@
+provider "vault" {
+  address = data.terraform_remote_state.vault.outputs.fqdn
+}
+
 data "vault_approle_auth_backend_role_id" "approle_auth_backend_role_id" {
   role_name = var.vault_role
 }
